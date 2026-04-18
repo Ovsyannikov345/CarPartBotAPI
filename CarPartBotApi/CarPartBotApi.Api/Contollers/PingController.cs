4859a9d9501b25e603dd42b94badbce36759a76a
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2026-04-18 23:40:07 +0300

    Add ping endpoint

---
+++ CarPartBotApi/CarPartBotApi.Api/Contollers/PingController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarPartBotApi.Api.Contollers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PingController(IHostEnvironment _hostEnvironment) : ControllerBase
+{
+    /// <summary>
+    /// A simple endpoint to verify the API is up and responding to HTTP requests.
+    /// </summary>
+    [HttpGet]
+    public IActionResult Ping() // TODO Protect
+    {
+        return Ok(new
+        {
+            message = "pong",
+            timestamp = DateTimeOffset.UtcNow,
+            environment = _hostEnvironment.EnvironmentName
+        });
+    }
+}
